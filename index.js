12cad7f24d23784c130110b8f6fcf043cde63ff4
harjas816 <harjas816@gmail.com>
2023-06-02 02:01:46 -0400

    complete

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let chars = []
+  let comparator = ""
+  for (i of word){
+    chars.push(i)
+    }
+  let charsReverse = chars.reverse()
+  for (i of charsReverse){
+    comparator = comparator + i
+  }
+  if (comparator == word){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  
+  make a list (chars) to push string characters in 
+  make a string (comparator) to eventually concatneate 
+  for(the characters in the string){
+    push them into chars
+  }
+  make a list that is just chars but in reverse order
+  for(everything in this new reversed chars list){
+    concatenate characters with comparator
+  }
+  if (comparator matches word exactly){
+    return true
+  }
+  else (if comparator doesn't match word){
+    return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+  I took the given string (in this case the parameter "word"),
+  and put all of its characters into a list. I then reversed that list,
+  and added all the characters of the reverse list to an empty string in order to compare it to the given word.
 */
 
 // You can run `node index.js` to view these console logs
